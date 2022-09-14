57a6b26f7f7fc189d6485fac2eee7098665a8aa2
ravi <ravi.shankar@jarvis.consulting>
2022-09-14 18:15:12 +0530

    3rd commit

--- src/component/prominent_detail.jsx
+++ src/component/prominent_detail.jsx
@@ -30,11 +30,11 @@ export default function Detail() {
                 {({values, setFieldValue}) => (
                     <Formstepper>
 
-                        <div class="container text-center">
+                        <div className="container text-center">
                             <h4>Personal Detail</h4>
-                            <div class="row justify-content-start ">
-                                <div class={"col p-3"}>
-                                    <Field class="form-control form-control-lg" name="name" placeholder={"Name"}
+                            <div className="row justify-content-start ">
+                                <div className={"col p-3"}>
+                                    <Field className="form-control form-control-lg" name="name" placeholder={"Name"}
                                            required={true}/><br></br>
                                 </div>
 
@@ -43,19 +43,19 @@ export default function Detail() {
                                         setFieldValue("file", event.target.files[0])
                                     }}/>
                                     {values.file && <Preview file={values.file}/>}
-                                    <button class={"btn btn-danger"} onClick={() => {
+                                    <button className={"btn btn-danger"} onClick={() => {
                                         fileRef.current.click();
                                     }}>Attach Picture
                                     </button>
                                 </div>
                             </div>
 
-                            <div class="row align-items-start">
-                                <div class="col p-3">
-                                    <Field class="form-control form-control-lg" name="gender" placeholder={"Gender"}/>
+                            <div className="row align-items-start">
+                                <div className="col p-3">
+                                    <Field className="form-control form-control-lg" name="gender" placeholder={"Gender"}/>
                                 </div>
-                                <div class={"col p-3"}>
-                                    <Field class="form-control form-control-lg" as={"select"} name="religion" placeholder={"Religion"}>
+                                <div className={"col p-3"}>
+                                    <Field className="form-control form-control-lg" as={"select"} name="religion" placeholder={"Religion"}>
                                         <option value="">Religion</option>
                                         <option value="Hindu">HINDU</option>
                                         <option value="Islam">Islam</option>
@@ -65,38 +65,38 @@ export default function Detail() {
                                     </Field>
                                 </div>
                             </div>
-                            <div class={"row align-items-start"}>
-                                <div class={"col p-3"}>
-                                    <Field class="form-control form-control-lg" name="category" placeholder={"Category"}/>
+                            <div className={"row align-items-start"}>
+                                <div className={"col p-3"}>
+                                    <Field className="form-control form-control-lg" name="category" placeholder={"Category"}/>
                                 </div>
-                                <div class={"col p-3"}>
-                                    <Field class="form-control form-control-lg" name="dob" placeholder={"D.O.B"}/><br></br>
+                                <div className={"col p-3"}>
+                                    <Field className="form-control form-control-lg" name="dob" placeholder={"D.O.B"}/><br></br>
                                 </div>
 
                             </div>
-                            <div class={"row align-items-start"}>
-                                <div class={"col p-3"}>
-                                    <Field class="form-control form-control-lg" name="father" placeholder={"Father"}/>
+                            <div className={"row align-items-start"}>
+                                <div className={"col p-3"}>
+                                    <Field className="form-control form-control-lg" name="father" placeholder={"Father"}/>
                                 </div>
-                                <div class={"col p-3"}>
-                                    <Field class="form-control form-control-lg" name="mother" placeholder={"Mother"}/><br></br>
+                                <div className={"col p-3"}>
+                                    <Field className="form-control form-control-lg" name="mother" placeholder={"Mother"}/><br></br>
                                 </div>
                             </div>
 
-                            <div class={"row align-items-start"}>
-                                <div class={"col p-3"}>
-                                    <Field class="form-control form-control-lg" name="spouse" placeholder={"Spouse"}/>
+                            <div className={"row align-items-start"}>
+                                <div className={"col p-3"}>
+                                    <Field className="form-control form-control-lg" name="spouse" placeholder={"Spouse"}/>
                                 </div>
-                                <div class={"col p-3"}>
-                                    <Field class="form-control form-control-lg" name="child" placeholder={"Child"}/>
+                                <div className={"col p-3"}>
+                                    <Field className="form-control form-control-lg" name="child" placeholder={"Child"}/>
                                 </div>
                             </div>
                         </div>
 
-                        <div class="container text-center">
+                        <div className="container text-center">
                             <h4>Communication Detail</h4>
-                            <div class="row align-items-start">
-                                <div class={"col"}>
+                            <div className="row align-items-start">
+                                <div className={"col"}>
                                     <FieldArray
                                         name="mobile"
                                         render={arrayHelpers => (
@@ -104,7 +104,7 @@ export default function Detail() {
                                                 {values.mobile && values.mobile.length > 0 ? (
                                                     values.mobile.map((phone, index) => (
                                                         <div key={index}>
-                                                            <Field class="form-control form-control-lg" name={`mobile.${index}`}
+                                                            <Field className="form-control form-control-lg" name={`mobile.${index}`}
                                                                    type={"number"} placeholder={"Enter Number"}/>
                                                             <button
                                                                 type="button"
@@ -121,7 +121,7 @@ export default function Detail() {
                                                         </div>
                                                     ))
                                                 ) : (
-                                                    <button class="btn btn-secondary"
+                                                    <button className="btn btn-secondary"
                                                             onClick={() => arrayHelpers.push('')}>
                                                         Add Mobile
                                                     </button>
@@ -131,28 +131,28 @@ export default function Detail() {
                                         )}
                                     />
                                 </div>
-                                <div class={"col"}>
-                                    <Field class="form-control form-control-lg" name="email" placeholder="Email"/>
+                                <div className={"col"}>
+                                    <Field className="form-control form-control-lg" name="email" placeholder="Email"/>
                                 </div>
                                 {/*<Field name="email" placeholder="Email"/>*/}
                             </div>
                         </div>
-                        <div class="container text-center">
+                        <div className="container text-center">
                             <h4>Education Detail</h4>
-                            <div class={"row align-items-start"}>
-                                <div class={"col p-3"}>
-                                    <Field class="form-control form-control-lg" name="degree" placeholder={"Degree"}/>
+                            <div className={"row align-items-start"}>
+                                <div className={"col p-3"}>
+                                    <Field className="form-control form-control-lg" name="degree" placeholder={"Degree"}/>
                                 </div>
-                                <div class={"col p-3"}>
-                                    <Field class="form-control form-control-lg" name="course" placeholder={"Subject"}/><br></br>
+                                <div className={"col p-3"}>
+                                    <Field className="form-control form-control-lg" name="course" placeholder={"Subject"}/><br></br>
                                 </div>
                             </div>
-                            <div class={"row align-items-start"}>
-                                <div class={"col p-3"}>
-                                    <Field class="form-control form-control-lg" name="startyear" placeholder={"StartYear"}/>
+                            <div className={"row align-items-start"}>
+                                <div className={"col p-3"}>
+                                    <Field className="form-control form-control-lg" name="startyear" placeholder={"StartYear"}/>
                                 </div>
-                                <div class={"col p-3"}>
-                                    <Field class="form-control form-control-lg" name="endyear" placeholder={"Endyear"}/>
+                                <div className={"col p-3"}>
+                                    <Field className="form-control form-control-lg" name="endyear" placeholder={"Endyear"}/>
                                 </div>
 
                             </div>
@@ -160,7 +160,7 @@ export default function Detail() {
                         </div>
 
                         <div>
-                            <button class="btn btn-primary" type="submit">Submit</button>
+                            <button className="btn btn-primary" type="submit">Submit</button>
                         </div>
                     </Formstepper>
                 )}
