24fe0b8cd943f6b7ba316de791ff8d92962cfedd
ravi <ravi.shankar@jarvis.consulting>
2022-09-16 10:13:59 +0530

    4th commit

--- src/component/Formstepper.jsx
+++ src/component/Formstepper.jsx
@@ -1,23 +1,23 @@
-import React, { useState } from "react";
-import { Form } from "formik";
-import { Button, Stack, Step, StepLabel, Stepper } from "@mui/material";
+import React, {useState} from "react";
+import {Form} from "formik";
+import {Button, Stack, Step, StepLabel, Stepper} from "@mui/material";
 
-export const Formstepper = ({ children }) => {
+export const Formstepper = ({children}) => {
     const stepsArray = React.Children.toArray(children);
     const [step, setStep] = useState(0);
     const currentStep = stepsArray[step];
 
-    function SttaperToNext () {
-     setStep(step + 1)
+    function StepperToNext() {
+        setStep(step + 1)
     }
 
-    function SttaperToBack () {
+    function StepperToBack() {
         setStep(step - 1)
     }
 
     return (
         <Form>
-            <Stepper  alternativeLabel activeStep={step} sx={{ marginBottom: 5 }}>
+            <Stepper alternativeLabel activeStep={step} sx={{marginBottom: 5}}>
                 {stepsArray.map((child, index) => (
                     <Step key={index} completed={step > index}>
                         <StepLabel>{}</StepLabel>
@@ -27,32 +27,36 @@ export const Formstepper = ({ children }) => {
             </Stepper>
             {currentStep}
 
-            <Stack direction="row" spacing={2} sx={{ marginTop: 5 }}>
-                {step !== 0 ?
-                    <Button
-                    variant="outlined"
-                    onClick={() => {
-                        SttaperToBack()
-                    }}
-                >
-                 Back
-                </Button> : ""}
+            <div className="fixed-bottom">
+                <Stack direction="row" spacing={2} sx={{marginTop: 5}}>
+                    {step !== 0 ?
+                        <Button
+                            style={{backgroundColor:"orangered" ,color:"whitesmoke"}}
+                            variant="outlined"
+                            onClick={() => {
+                                StepperToBack()
+                            }}
+                        >
+                            Back
+                        </Button> : ""}
 
-                {step === 3 ? "" :
-                <Button
-                    variant="outlined"
-                    onClick={() => {
-                        SttaperToNext()
-                    }}
-                >
-                    Next
-                </Button>}
-                {step === 4 && (
-                    <Button variant="contained" type="submit">
-                        Submit
-                    </Button>
-                )}
-            </Stack>
+                    {step === 3 ? "" :
+                        <Button
+                            style={{backgroundColor:"orangered" ,color:"whitesmoke"}}
+                            variant="outlined"
+                            onClick={() => {
+                                StepperToNext()
+                            }}
+                        >
+                            Next
+                        </Button>}
+                    {step === 4 && (
+                        <Button variant="contained" type="submit" >
+                            Submit
+                        </Button>
+                    )}
+                </Stack>
+            </div>
         </Form>
     );
 };
--- src/component/prominent_detail.jsx
+++ src/component/prominent_detail.jsx
@@ -10,6 +10,19 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Detail() {
     const fileRef = useRef(null);
+
+    async function AddData(values) {
+        let result = await fetch( "http://localhost:3000/api/detail/prominent", {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": 'application/json',
+            },
+            body: JSON.stringify(values)
+        });
+        result = await result.json();
+        console.log("data",result)
+    }
     return (
         <div>
             {/*<Pic></Pic>*/}
@@ -18,11 +31,13 @@ export default function Detail() {
                 initialValues={{
                     file: "",
                     name: "", gender: "", religion: "", category: "", dob: "", father: "",
-                    mother: "", spouse: "", child: "", degree: "", course: "", startyear: "",
-                    endyear: "", mobile: []
+                    mother: "", spouse: "", child: "",
+                    education: [{degree: "", course: "", startyear: "", endyear: ""}],
+                    mobile:[]
                 }}
                 onSubmit={(values) =>
                     setTimeout(() => {
+                        AddData(values)
                         console.log(JSON.stringify(values, null, 2));
                         console.log(values)
                     }, 500)
@@ -34,8 +49,11 @@ export default function Detail() {
                             <h4>Personal Detail</h4>
                             <div className="row justify-content-start ">
                                 <div className={"col p-3"}>
-                                    <Field className="form-control form-control-lg" name="name" placeholder={"Name"}
-                                           required={true}/><br></br>
+                                    <div className="form-floating mb-0">
+                                        <Field className="form-control form-control-lg" name="name" placeholder={"Name"}
+                                               required={true} id={"floatingInputValue"}/>
+                                        <label htmlFor="floatingTextarea">Name</label>
+                                    </div>
                                 </div>
 
                                 <div className="col p-3">
@@ -51,11 +69,24 @@ export default function Detail() {
                             </div>
 
                             <div className="row align-items-start">
+
                                 <div className="col p-3">
-                                    <Field className="form-control form-control-lg" name="gender" placeholder={"Gender"}/>
+                                    {/*<div className="form-floating mb-0">*/}
+                                    <Field className="form-control form-control-lg" name="gender"
+                                           placeholder={"Gender"} as={"select"}>
+                                        <option value="">Gender</option>
+                                        <option value="MALE">MALE</option>
+                                        <option value="FEMALE">FEMALE</option>
+                                        <option value="OTHER">OTHER</option>
+                                    </Field>
+
+                                    {/*<label htmlFor="floatingTextarea">Gender</label>*/}
+                                    {/*</div>*/}
                                 </div>
+
                                 <div className={"col p-3"}>
-                                    <Field className="form-control form-control-lg" as={"select"} name="religion" placeholder={"Religion"}>
+                                    <Field className="form-control form-control-lg" as={"select"} name="religion"
+                                           placeholder={"Religion"}>
                                         <option value="">Religion</option>
                                         <option value="Hindu">HINDU</option>
                                         <option value="Islam">Islam</option>
@@ -64,28 +95,34 @@ export default function Detail() {
                                         <option value="Christianity">Christianity</option>
                                     </Field>
                                 </div>
+
                             </div>
                             <div className={"row align-items-start"}>
-                                <div className={"col p-3"}>
-                                    <Field className="form-control form-control-lg" name="category" placeholder={"Category"}/>
+                                <div className={"col p-3 "}>
+                                    <Field className="form-control form-control-lg " name="category"
+                                           placeholder={"Category"}/>
                                 </div>
                                 <div className={"col p-3"}>
-                                    <Field className="form-control form-control-lg" name="dob" placeholder={"D.O.B"}/><br></br>
+                                    <Field className="form-control form-control-lg" name="dob"
+                                           type={"Date"}/>
                                 </div>
 
                             </div>
                             <div className={"row align-items-start"}>
                                 <div className={"col p-3"}>
-                                    <Field className="form-control form-control-lg" name="father" placeholder={"Father"}/>
+                                    <Field className="form-control form-control-lg" name="father"
+                                           placeholder={"Father"}/>
                                 </div>
                                 <div className={"col p-3"}>
-                                    <Field className="form-control form-control-lg" name="mother" placeholder={"Mother"}/><br></br>
+                                    <Field className="form-control form-control-lg" name="mother"
+                                           placeholder={"Mother"}/><br></br>
                                 </div>
                             </div>
 
                             <div className={"row align-items-start"}>
                                 <div className={"col p-3"}>
-                                    <Field className="form-control form-control-lg" name="spouse" placeholder={"Spouse"}/>
+                                    <Field className="form-control form-control-lg" name="spouse"
+                                           placeholder={"Spouse"}/>
                                 </div>
                                 <div className={"col p-3"}>
                                     <Field className="form-control form-control-lg" name="child" placeholder={"Child"}/>
@@ -97,65 +134,135 @@ export default function Detail() {
                             <h4>Communication Detail</h4>
                             <div className="row align-items-start">
                                 <div className={"col"}>
-                                    <FieldArray
-                                        name="mobile"
-                                        render={arrayHelpers => (
+                                    <FieldArray name="mobile">
+
+                                        {({insert, remove, push}) => (
                                             <div>
-                                                {values.mobile && values.mobile.length > 0 ? (
+                                                {values.mobile.length > 0 &&
                                                     values.mobile.map((phone, index) => (
                                                         <div key={index}>
-                                                            <Field className="form-control form-control-lg" name={`mobile.${index}`}
+                                                            {/*<Field  name={`mobile.${index}`}/>*/}
+                                                            <Field className="form-control form-control-lg"
+                                                                   name={`mobile.${index}`}
                                                                    type={"number"} placeholder={"Enter Number"}/>
+
                                                             <button
                                                                 type="button"
-                                                                onClick={() => arrayHelpers.remove(index)} // remove a friend from the list
+                                                                onClick={() => remove(index)} // remove a mobile from the list
                                                             >
                                                                 <DeleteOutlineOutlinedIcon fontSize={"small"}/>
                                                             </button>
-                                                            <button
-                                                                type="button"
-                                                                onClick={() => arrayHelpers.insert(index, '')} // insert an empty string at a position
-                                                            >
-                                                                <AddCircleOutlinedIcon fontSize="small"/>
-                                                            </button>
+                                                            {/*<button*/}
+                                                            {/*    type="button"*/}
+                                                            {/*    onClick={() => insert(index, '')} // insert an empty string at a position*/}
+                                                            {/*>*/}
+                                                            {/*    <AddCircleOutlinedIcon fontSize="small"/>*/}
+                                                            {/*</button>*/}
                                                         </div>
-                                                    ))
-                                                ) : (
-                                                    <button className="btn btn-secondary"
-                                                            onClick={() => arrayHelpers.push('')}>
-                                                        Add Mobile
-                                                    </button>
-                                                )
-                                                }
+                                                    ))}
+                                                <button className="btn btn-secondary"
+                                                        onClick={() => push('')}>
+                                                    <AddCircleOutlinedIcon fontSize="small"/>
+                                                    {/*Add mob*/}
+                                                </button>
                                             </div>
                                         )}
-                                    />
+                                    </FieldArray>
                                 </div>
                                 <div className={"col"}>
                                     <Field className="form-control form-control-lg" name="email" placeholder="Email"/>
                                 </div>
-                                {/*<Field name="email" placeholder="Email"/>*/}
                             </div>
                         </div>
+
                         <div className="container text-center">
                             <h4>Education Detail</h4>
-                            <div className={"row align-items-start"}>
-                                <div className={"col p-3"}>
-                                    <Field className="form-control form-control-lg" name="degree" placeholder={"Degree"}/>
-                                </div>
-                                <div className={"col p-3"}>
-                                    <Field className="form-control form-control-lg" name="course" placeholder={"Subject"}/><br></br>
-                                </div>
-                            </div>
-                            <div className={"row align-items-start"}>
-                                <div className={"col p-3"}>
-                                    <Field className="form-control form-control-lg" name="startyear" placeholder={"StartYear"}/>
-                                </div>
-                                <div className={"col p-3"}>
-                                    <Field className="form-control form-control-lg" name="endyear" placeholder={"Endyear"}/>
-                                </div>
+                            {/*<div className={"row align-items-start"}>*/}
+                            {/*    <div className={"col p-3"}>*/}
+                            {/*        <Field className="form-control form-control-lg" name="degree"*/}
+                            {/*               placeholder={"Degree"}/>*/}
+                            {/*    </div>*/}
+                            {/*    <div className={"col p-3"}>*/}
+                            {/*        <Field className="form-control form-control-lg" name="course"*/}
+                            {/*               placeholder={"Subject"}/><br></br>*/}
+                            {/*    </div>*/}
+                            {/*</div>*/}
+                            {/*<div className={"row align-items-start"}>*/}
+                            {/*    <div className={"col p-3"}>*/}
+                            {/*        <Field className="form-control form-control-lg" name="startyear"*/}
+                            {/*               placeholder={"StartYear"}/>*/}
+                            {/*    </div>*/}
+                            {/*    <div className={"col p-3"}>*/}
+                            {/*        <Field className="form-control form-control-lg" name="endyear"*/}
+                            {/*               placeholder={"Endyear"}/>*/}
+                            {/*    </div>*/}
 
-                            </div>
+                            {/*</div>*/}
+
+                            <FieldArray name="education">
+                                {({insert, remove, push}) => (
+                                    <div>
+                                        {values.education.length > 0 &&
+                                            values.education.map((edu, index) => (
+                                                <div key={index}>
+                                                    {/*<Field  name={`mobile.${index}`}/>*/}
+                                                    <div className="row align-items-start">
+                                                        <div className={"col"}>
+                                                            <Field className="form-control form-control-lg"
+                                                                   name={`education.${index}.degree`}
+                                                                   type={"text"} placeholder={"Degree"}/>
+                                                        </div>
+                                                        <div className={"col"}>
+                                                            <Field className="form-control form-control-lg"
+                                                                   name={`education.${index}.course`}
+                                                                   type={"text"} placeholder={"Course"}/>
+                                                        </div>
+
+                                                    </div>
+                                                    <div className="row align-items-start">
+                                                        <div className={"col"}>
+                                                            <Field className="form-control form-control-lg"
+                                                                   name={`education.${index}.startyear`}
+                                                                   type={"text"} placeholder={"Startyear"}/>
+                                                        </div>
+                                                        <div className={"col"}>
+                                                            <Field className="form-control form-control-lg"
+                                                                   name={`education.${index}.endyear`}
+                                                                   type={"text"} placeholder={"Endyear"}/>
+                                                        </div>
+
+                                                    </div>
+
+
+                                                    <button
+                                                        type="button"
+                                                        onClick={() => remove(index)} // remove a mobile from the list
+                                                    >
+                                                        <DeleteOutlineOutlinedIcon fontSize={"small"}/>
+                                                    </button>
+                                                    {/*<button*/}
+                                                    {/*    type="button"*/}
+                                                    {/*    onClick={() => insert(index, '')} // insert an empty string at a position*/}
+                                                    {/*>*/}
+                                                    {/*    <AddCircleOutlinedIcon fontSize="small"/>*/}
+                                                    {/*</button>*/}
+                                                </div>
+                                            ))}
+                                        <button className="btn btn-secondary"
+                                                onClick={() => push({
+                                                    degree: "",
+                                                    course: "",
+                                                    startyear: "",
+                                                    endyear: ""
+                                                })}>
+                                            <AddCircleOutlinedIcon fontSize="small"/>
+                                            {/*Add mob*/}
+                                        </button>
+
+                                    </div>
+                                )}
+                            </FieldArray>
+
 
                         </div>
 
